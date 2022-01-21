c28ce75d7e787d21005afe4933a4a5136d454e9a
MildaPau <pauzaite.milda@gmail.com>
2022-01-21 14:33:27 +0200

    append

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var inputMetai = document.getElementsByTagName("input")[2];
 var btnPrideti = document.getElementsByTagName("button")[0];
 var btnIstrinti1 = document.getElementsByTagName("button")[1];
 var btnIstrinti2 = document.getElementsByTagName("button")[2];
+
 var table = document.getElementsByTagName("table")[0];
 var tr = document.getElementsByTagName("tr");
 
@@ -17,22 +18,19 @@ btnPrideti.addEventListener("click", function(){
         } else if (inputMetai.value <= 0){
             alert("Įvestas skaičius turi būti didesnis už 0.");
         }else {
-            table;
 
-            var row = table.insertRow();
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var cell3 = row.insertCell(2);
-            cell1.innerHTML = `<td>${inputVardas.value}</td>`;
-            cell2.innerHTML = `<td>${inputPavarde.value}</td>`;
-            cell3.innerHTML = `<td>${inputMetai.value}</td>`;
+        var newName = document.createElement("tr");
+        newName.innerHTML = `<td>${inputVardas.value}</td><td>${inputPavarde.value}</td><td>${inputMetai.value}</td>`;
+        table.append(newName);
 
-            inputVardas.value = "";
-            inputPavarde.value = "";
-            inputMetai.value = "";
+        inputVardas.value = "";
+        inputPavarde.value = "";
+        inputMetai.value = "";
 
-            
-            btnIstrinti1.addEventListener("click", function(){
+        }
+})
+
+    btnIstrinti1.addEventListener("click", function(){
                 if(tr.length == 0){
                     alert("Nėra ką trinti!");
                 } else {
@@ -46,19 +44,7 @@ btnPrideti.addEventListener("click", function(){
                 } else{
                     tr[tr.length - 1].remove();
                 }
-            })
-        
+    })
+
+
             
-        // var td = document.createElement("td");
-        // td.innerHTML = `<td>${inputVardas.value}</td>`;
-        
-        // var td1 = document.createElement("td");
-        // td1.innerHTML = `<td>${inputPavarde.value}</td>`;
-
-        // var td2 = document.createElement("td");
-        // td2.innerHTML = `<td>${inputMetai.value}</td><br>`;
-        // table.append(td, td1, td2);
-
-        }
-        
-})
